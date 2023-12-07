d1d6f951f98453d48a9083f429dfc08739976ee9
Bianca Janotova <byancajanotova@gmail.com>
2023-12-07 22:16:57 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def code(s:str,p:int):
+    sifra=""
+    for znak in s:
+        znak_code = org(znak)
+        novy_znak_code = znak_code + p
+        novy_znak = chr(nova_znak_code)
+        return = return + novy_znak
+    print(return)
+print(Hello, 3)
+
